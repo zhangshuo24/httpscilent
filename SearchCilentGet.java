c19006ca467ce1ddce9b77067b14cc4f9cd98019
zhangshuo24 <15045348836@163.com>
2017-05-18 13:56:02 +0800

    修改私密信息
    修改敏感数据

--- SearchCilentGet.java
+++ SearchCilentGet.java
@@ -6,14 +6,6 @@ package com.demo.cilent;
  */
 public class SearchCilentGet {
 	public static void main(String[] args) throws Exception {
-		/**
-		 * 
-		 * https://test.biinfo.cn/biinfoservice/icbc/finst 一审数据api调用地址
-		 * 
-		 * 比如：https://test.biinfo.cn/biinfoservice/icbc/finst?apiKey=分配用户的apikey
-		 */
-		String httpUrl="https://wx.biinfo.cn/biinfoservice/icbc/finst?apiKey=zupWLKvjSlcesrdgkNXmOAMhVeOPHYYSnefieLwgUAFcYKiH&page=1";
-		String result=SearchTools.sendHttpsGet(httpUrl);
-		//System.out.println(result);
+
 	}
 }
